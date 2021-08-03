73e9f17a0fc3b787450660820d78f93068854233
YashNarK <narenkrithick@gmail.com>
2021-08-04 01:26:38 +0530

    settings.py has domain included in STATICFILES_DIR

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'var','static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),'var/www/static']
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),'narendranai.pythonanywhere.com/static','/var/www/static']
 
 
 # Default primary key field type
